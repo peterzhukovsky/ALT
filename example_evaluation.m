69bbb60eda3cfaa63ce4d6d8ad2727211c87414f
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2022-01-28 12:20:17 -0500

    Update example_evaluation.m

--- example_evaluation.m
+++ example_evaluation.m
@@ -7,17 +7,15 @@
 %for i in `ls -d *.ica`; do cat $i/filtered_func_data.ica/labels2_c.txt | tr -s '\[' ' ' > $i/filtered_func_data.ica/labels2_cut.txt ; done
 %for i in `ls -d *.ica`; do cat $i/filtered_func_data.ica/labels2_cut.txt | tr -s '\]' ' ' > $i/filtered_func_data.ica/labels2_c.txt ; done
 
-
-%% next, in matlab or octave calculate the agreement with manual labeling using the cofusion matrix
-
-addpath('<path to alt>\ALT');
-
 %navigate to the directory where the MELODIC processed data is located and find all subject folders
 %below script works for directories with the structure: 
 % sub-001/filtered_func_data.ica
 % sub-002/filtered_func_data.ica
 % ...
   
+%% next, in matlab or octave calculate the agreement with manual labeling using the cofusion matrix
+addpath('<path to alt>\ALT');
+
 folders=cellstr(ls); % 
 
 for i=1:length(folders)
